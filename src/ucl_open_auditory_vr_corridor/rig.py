e892df005f9df6ab7016dee19f86ab6f768a6a24
neurochilds <childsy123@gmail.com>
2026-05-13 15:43:43 +0100

    Added lickity split + GUI

--- src/ucl_open_auditory_vr_corridor/rig.py
+++ src/ucl_open_auditory_vr_corridor/rig.py
@@ -4,6 +4,7 @@ from pydantic import Field
 from ucl_open.rigs.base import BaseSchema
 import ucl_open.rigs.device as Device
 import ucl_open.rigs.controllers as Controllers
+from ucl_open.rigs.video import SpinnakerCamera
 
 from ucl_open_auditory_vr_corridor import __semver__
 
@@ -11,3 +12,7 @@ class UclOpenAuditoryVrCorridorRig(BaseSchema):
     version: Literal[__semver__] = __semver__
     behaviorboard: Device.BehaviorBoard
     runningwheel: Controllers.RunningWheelModule
+    lickety_splits:  Device.LicketySplit
+    cameras: SpinnakerCamera
+    led_driver: Device.ArduinoDevice
+    #stepper_motors: StepperMotor
